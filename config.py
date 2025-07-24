bc44a7fc546dfbb80207a24c1a239780c3b32731
Raven <157051380+raven-elizabeth@users.noreply.github.com>
2025-07-24 11:21:31 +0100

    Altered names to reflect actual types & added misc

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 downloads = {
     "windows": "Downloads", # Rename if your downloads folder is called something else
-    "folders": ["Images", "Videos", "Audio", "Documents", "Applications"] # Edit sub-folder names if necessary
+    "folders": ["Image", "Video", "Audio", "Document", "Application", "Misc"] # Edit sub-folder names if necessary (may break code)
 }
 
 # Could add language choice later
