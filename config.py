48c91e7ad4784740cb6bcafe38bb56138cd85ec9
Raven <157051380+raven-elizabeth@users.noreply.github.com>
2025-07-24 12:14:07 +0100

    Added lists to store installer and code extensions

--- config.py
+++ config.py
@@ -4,8 +4,11 @@ downloads = {
                 {"video": "Videos"},
                 {"audio": "Audio"},
                 {"application", "Documents"},
-                {"misc": "Misc"}],
-    "installers": [] # These files will be deleted after a set time period
+                {"misc": "Misc"},
+                {"code": "Code"}]
 }
+
+installers = [".exe", ".msi", ".pkg", ".dmg", ".deb", ".rpm"] # These files will be deleted after a set time period (intended use for setup/installer files that are no longer needed)
+code_programs = [".py", ".js", ".java", ".sql", ".cs", ".php", ".go", ".mongodb", ".ts", ".rb", ".cpp", ".c"] # Add extensions for code/database files here
 
 # Could add language choice later
