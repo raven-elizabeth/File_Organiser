e638cc6a6e06c9bdb0396b35cba4bce7b05d25dc
Raven <157051380+raven-elizabeth@users.noreply.github.com>
2025-07-24 13:15:25 +0100

    Added folder for exectuables/installers

--- config.py
+++ config.py
@@ -5,10 +5,14 @@ downloads = {
                 {"audio": "Audio"},
                 {"application", "Documents"},
                 {"misc": "Misc"},
-                {"code": "Code"}]
+                {"code": "Code"},
+                {"installers": "Setup Files"}]
 }
 
-installers = [".exe", ".msi", ".pkg", ".dmg", ".deb", ".rpm"] # These files will be deleted after a set time period (intended use for setup/installer files that are no longer needed)
 code_programs = [".py", ".js", ".java", ".sql", ".cs", ".php", ".go", ".mongodb", ".ts", ".rb", ".cpp", ".c"] # Add extensions for code/database files here
+
+
+installers = [".exe", ".msi", ".pkg", ".dmg", ".deb", ".rpm"] # These files will be deleted after a set time period (intended use for setup/installer files that are no longer needed)
+installer_deletion_countdown = 1209600 # Default value 1,209,600 is 2 weeks
 
 # Could add language choice later
